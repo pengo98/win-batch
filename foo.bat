b2fbfcbaf36d3a8401a07a477ab4d1bc00029f8d
nelson.lin <nelson.lin@jet.com>
2017-10-29 15:33:36 -0400

    use code block and redirection to write to file

--- foo.bat
+++ foo.bat
@@ -25,4 +25,11 @@ mkdir ".fake"
 powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v3.5.0/NuGet.exe -OutFile .fake\NuGet.exe"
 ".fake\NuGet.exe" "Install" "FAKE" "-Version" "4.50" "-OutputDirectory" ".fake\packages" "-ExcludeVersion"
 
+@echo off
+(
+echo cls
+echo cd %%~dp0
+echo ".fake\packages\FAKE\tools\Fake.exe" %%* --nocache
+) > fake.bat
+@echo on
 
