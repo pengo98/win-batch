215cf6096a040dcb0d97324786dc2cd9dac5e549
nelson.lin <nelson.lin@jet.com>
2017-10-29 00:07:37 -0400

    initial

---
+++ foo.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enableextensions
+set me=%~n0
+set parent=%~dp0
+
+cls
+echo %date%
+cd %~dp0
+echo %me% 
+echo %parent%
+set foo=bar
+echo %foo%
+echo errorlevel=%errorlevel%
+
+if exist "fake.bat" ( 
+    del "fake.bat"
+)
+
+if exist ".fake" ( 
+    rmdir /s /q ".fake"
+)
+mkdir ".fake"
+
+goto comment
+powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v3.5.0/NuGet.exe -OutFile .fake\NuGet.exe"
+".fake\NuGet.exe" "Install" "FAKE" "-Version" "4.50" "-OutputDirectory" ".fake\packages" "-ExcludeVersion"
+
+@echo off
+(
+echo cls
+echo cd %%~dp0
+echo ".fake\packages\FAKE\tools\Fake.exe" %%* --nocache
+) > fake.bat
+@echo on
+
+:comment
